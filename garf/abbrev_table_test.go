6d00da887facaca0c438d16bab1c6b5452beb8c1
sivachandra <sivachandra@google.com>
2016-04-06 06:44:01 -0700

    Change the license to Apache License Version 2.0.

--- garf/abbrev_table_test.go
+++ garf/abbrev_table_test.go
@@ -1,10 +1,18 @@
-// #############################################################################
-// This file is part of the "garf" package of the "Eureka" project.
-// It is distributed under the MIT License. Refer to the LICENSE file for more
-// information.
+///////////////////////////////////////////////////////////////////////////
+// Copyright 2016 Siva Chandra
 //
-// Website: http://www.github.com/sivachandra/eureka
-// #############################################################################
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+///////////////////////////////////////////////////////////////////////////
 
 package garf
 
